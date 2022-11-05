1e6615a2c5b35134da8f7be72571197c62ba34b6
raafayvohra <raafayvohra.mrv@gmail.com>
2022-11-05 13:29:57 +0500

    created a task list

---
+++ app.js
@@ -0,0 +1,48 @@
+
+//Adds a task to the list
+function addTask() {
+
+
+    let taskContainer = document.querySelector('.task-container');
+    let input = document.querySelector('#input');
+
+    if (input.value === '') {
+        alert('Please enter some text');
+    } else {
+
+
+        let task = document.createElement('div');
+        task.className = 'task';
+
+        let text = document.createElement('p');
+        text.innerText = input.value;
+
+        let rmvDiv = document.createElement('div');
+        rmvDiv.className = 'remove-button';
+        let rmvButton = document.createElement('i');
+        rmvButton.classList.add('fa-solid', 'fa-xmark');
+        rmvDiv.appendChild(rmvButton)
+
+        task.append(text);
+        task.append(rmvDiv);
+
+        taskContainer.append(task);
+
+
+        input.value = '';
+    }
+}
+
+
+//Removes a single task from list
+function removeSingleTask() {
+    let taskContainer = document.querySelector('.task-container');
+
+}
+
+
+//clear all tasks from the list
+function clearTasks() {
+    let taskContainer = document.querySelector('.task-container');
+    taskContainer.innerHTML = '';
+}
